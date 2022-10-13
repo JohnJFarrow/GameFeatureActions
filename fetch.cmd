73ae129c1ee11a31e4ab436e2876fcc850d89c72
JohnJFarrow <105021048+JohnJFarrow@users.noreply.github.com>
2022-10-13 14:36:57 +1300

    renamed plugin

--- fetch.cmd
+++ fetch.cmd
@@ -1,7 +1,7 @@
 
 rem copy files from original repos
 
-robocopy ..\..\..\ValleyoftheAncient\Plugins\Systems\ModularGameplayActors . ModularGameplayActors.uplugin
+robocopy ..\..\..\ValleyoftheAncient\Plugins\Systems\ModularGameplayActors . GameFeatureActions.uplugin
 robocopy ..\..\..\ValleyoftheAncient\Plugins\Systems\ModularGameplayActors\Source Source /E
-robocopy ..\..\..\ValleyoftheAncient\Source\AncientGame\GameFeatures\ Source\ModularGameplayActors\Public *.h
-robocopy ..\..\..\ValleyoftheAncient\Source\AncientGame\GameFeatures\ Source\ModularGameplayActors\Private *.cpp
+robocopy ..\..\..\ValleyoftheAncient\Source\AncientGame\GameFeatures\ Source\GameFeatureActions\Public *.h
+robocopy ..\..\..\ValleyoftheAncient\Source\AncientGame\GameFeatures\ Source\GameFeatureActions\Private *.cpp
